73a9cea176b30ad151aeaf0d3c006175c0ad1042
yuxiu <yuxiu@showjoy.com>
2016-02-22 15:12:18 +0800

    fix remove comments function

--- bin/spon.js
+++ bin/spon.js
@@ -2,16 +2,12 @@
 var path = require('path');
 var fs = require('fs');
 var cli = require('commander');
-var appInfo = require('../package.json');
 var utils = require('../lib/utils');
 var chalk = require('chalk');
 var npmlog = require('npmlog');
 var _ = require('lodash');
 
 var log = console.log;
-var cyanLog = function(msg){
-    console.log(chalk.italic.cyan(msg));
-};
 
 var home = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
 var showjoyBase = path.join(home,'.spon');
@@ -21,36 +17,5 @@ if(!fs.existsSync(pluginsBase)){
 }
 
 var spon = require('../lib/spon-manager')(cli);
-
-/*
-cli
-    // .allowUnknownOption()//不报错误
-    .version(appInfo.version)
-    .usage('欢迎使用showjoy前端解决方案－spon <cmd> [subcmd]')
-    .action(function(cmd){
-        var cmds = ['init','mobi','mb','pc'];
-        var flag = false;
-        cmds.forEach(function(c){
-            if(c == cmd){
-                flag = true;
-            }
-        });
-        if(!flag){
-            npmlog.error('spon:mobi: ','sorry, do not have the command of "'+ cmd +'", please exacute "spon" to find something helpful!');
-            process.exit(1);
-        }
-    })
-    .parse(process.argv);
-
-cli
-    .command('init')
-    .description('初始化spon环境')
-    .action(function(){
-        spon.request('spon-env:init',{option: '', plugin: 'env',originOptions: ''})
-            .then(function(){
-                npmlog.info('spon:mobi: ','exec cmd: "spon init" successfully!');
-            },spon.fatal);
-    });
-*/
 
 cli.parse(process.argv);
